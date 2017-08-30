c0f468bc7a347b275ec07d889f69a3c19c1b2603
Arthur Prather <apratherjr@gmail.com>
2017-08-29 20:54:09 -0500

    added: partials ejs files, public directories, scss/bootstrap4

--- app.js
+++ app.js
@@ -2,14 +2,22 @@ var express = require('express');
 var app = express();
 
 //enable ejs
-app.set('view engines', 'ejs');
+app.set('view engine', 'ejs');
+
+//External Path
+app.use(express.static(__dirname + "/public"));
 
 //Routes
 
 
 //Home
 app.get('/', function(req, res){
-    res.send('home page');
+    res.render('home', {
+        nbr: '1',
+        name: 'Mercury',
+        desc: 'The 1st planet revloves 88 days around sun...',
+        list: ['Apple', 'Banana', 'Orange']
+    });
 });
 
 app.get('/planets/:planets_numbers?', function(req, res){
