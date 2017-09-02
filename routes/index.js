2d9ad6f9dccb8b1fa0c1e18455ffc81a1fb8d308
Arthur Prather <apratherjr@gmail.com>
2017-09-01 22:37:10 -0500

    fix, add, and adjusting ejs, routes, template etc

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,42 @@
+var planetsJSON = require('../planets.json');
+
 //Home
 exports.home = function(req, res){
+    var planets = planetsJSON.planets;
     res.render('home', {
         title: 'Our Solar System',
+        planets: planets
     });
 };
 
 //Single Page
 exports.planetSingle = function(req, res){
-    var planets_number = req.params.planets_number;
-    res.send("Planet Number =" + planets_number);
+    var planets = planetsJSON.planets;
+    var planetNbr = req.params.planets_numbers;
+
+    if( planetNbr >= 1 && planetNbr <= 8){
+        var planet = planets[planetNbr - 1];
+        var title = planet.planet_name;
+
+        res.render('planet_single', {
+            planets : planets,
+            planet : planet,
+            title : title
+        });
+    } else {
+        res.render('error', {
+            planets : planets,
+            title : "404 Error"
+        });
+    }
 };
+
 
 //Error
 exports.error = function(req, res){
-    res.send("404 Error - Page Not Found");
+    var planets = planetsJSON.planets;
+    res.render('error', {
+        planets : planets,
+        title : "404 Error"
+    });
 };
